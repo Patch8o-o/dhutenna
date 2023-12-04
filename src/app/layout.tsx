a469e1ba6e24452543b77c0bc7bc3ed7a4fd34e1
lily2oo <jumpei102113@icloud.com>
2023-12-04 17:08:34 +0900

    typekitlodarer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
 import './globals.css';
+import TypekitLoader from "@/app/TypekitLoader";
 
 
 const inter = Inter({ subsets: ['latin'] });
@@ -17,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <TypekitLoader />
       <body className={inter.className}>{children}</body>
     </html>
   );
